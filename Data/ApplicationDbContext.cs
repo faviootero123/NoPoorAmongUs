d403b271f87662afbc7ad89bbb5816b871dfbb4c
John Barnett <johnb@cadenceig.com>
2022-06-19 07:44:52 -0600

    needed to overide some base models

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace SaucyCapstone.Data;
@@ -8,5 +9,15 @@ public class ApplicationDbContext : IdentityDbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<IdentityUser>().Property(x => x.Id).HasMaxLength(225);
+        modelBuilder.Entity<IdentityRole>().Property(x => x.Id).HasMaxLength(225);
+        modelBuilder.Entity<IdentityUserLogin<string>>().Property(x => x.ProviderKey).HasMaxLength(225);
+        modelBuilder.Entity<IdentityUserLogin<string>>().Property(x => x.LoginProvider).HasMaxLength(225);
+        modelBuilder.Entity<IdentityUserToken<string>>().Property(x => x.Name).HasMaxLength(112);
+        modelBuilder.Entity<IdentityUserToken<string>>().Property(x => x.LoginProvider).HasMaxLength(112);
     }
 }
