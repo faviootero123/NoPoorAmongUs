e51ac74f1c7e28e052393737db6d94815e6810aa
John Barnett <johnb@cadenceig.com>
2022-06-19 20:38:51 -0600

    fixed my oof lols

---
+++ SaucyCapstone/Program.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SaucyCapstone.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Add services to the container.
+services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
+
+services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = true;
+    options.Stores.MaxLengthForKeys = 128;
+}).AddEntityFrameworkStores<ApplicationDbContext>();
+
+services.AddDatabaseDeveloperPageExceptionFilter();
+services.AddRazorPages()
+    .AddRazorRuntimeCompilation();
+services.AddControllersWithViews();
+
+
+
+var app = builder.Build();
+
+var mvcBuilder = builder.Services.AddRazorPages();
+// Configure the HTTP request pipeline. AKA middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+    app.UseHttpsRedirection();
+    app.UseDeveloperExceptionPage();
+    mvcBuilder.AddRazorRuntimeCompilation();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+//Handles headers forwarded from nginx
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute();
+app.MapRazorPages();
+
+app.Run();
