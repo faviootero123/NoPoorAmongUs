390db3bbd9feea0409182d8ca1e028a0172bc47a
WilliamLyday <williamlyday@mail.weber.edu>
2022-06-23 12:16:15 -0600

    added rough navbar

---
+++ SaucyCapstone/Controllers/JudgeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SaucyCapstone.Controllers
+{
+    public class JudgeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SaucyCapstone/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SaucyCapstone.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
