54c744d975da4108223f6cb02e6b8b52911f7374
John Barnett <johnb@cadenceig.com>
2022-07-01 17:00:28 -0600

    Remove Student From roles

--- SaucyCapstone/Constants/Roles.cs
+++ SaucyCapstone/Constants/Roles.cs
@@ -3,7 +3,6 @@
 public class Roles
 {
     public const string Admin = "Admin";
-    public const string Student = "Student";
     public const string SocialWorker = "Social Worker";
     public const string Instructor = "Instructor";
     public const string Rater = "Rater";
