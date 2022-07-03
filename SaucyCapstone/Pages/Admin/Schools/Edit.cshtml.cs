a86c53b652fe648f35126058e5959ef84bb7effb
seanafoster <seanafoster12@gmail.com>
2022-07-03 15:46:58 -0600

    Adding CRUD pages through scaffolding; updating layout

---
+++ SaucyCapstone/Pages/Admin/Schools/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Data;
+using SaucyCapstone.Data;
+
+namespace SaucyCapstone.Pages.Admin.Schools
+{
+    public class EditModel : PageModel
+    {
+        private readonly SaucyCapstone.Data.ApplicationDbContext _context;
+
+        public EditModel(SaucyCapstone.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public School School { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Schools == null)
+            {
+                return NotFound();
+            }
+
+            var school =  await _context.Schools.FirstOrDefaultAsync(m => m.SchoolId == id);
+            if (school == null)
+            {
+                return NotFound();
+            }
+            School = school;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(School).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SchoolExists(School.SchoolId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool SchoolExists(int id)
+        {
+          return (_context.Schools?.Any(e => e.SchoolId == id)).GetValueOrDefault();
+        }
+    }
+}
