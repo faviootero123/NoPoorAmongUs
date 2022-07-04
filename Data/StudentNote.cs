756a19aebd71f86cf4f4f1eb240a64b31a045eee
Favio Otero <faviootero@mail.weber.edu>
2022-07-03 18:47:47 -0600

    Jk Now the models should be good

---
+++ Data/StudentNote.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data;
+public class StudentNote
+{
+    public int StudentNoteId { get; set; }
+    public Student Student { get; set; }
+    public FacultyMember FacultyMember {get; set; }
+    public string Content { get; set; }
+    public DateTime CreatedDate { get; set; }
+}
