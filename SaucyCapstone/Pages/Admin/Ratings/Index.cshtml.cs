5fac1659dfd99cc657c91f3f511a1a115fc4a864
WILL-K-ALLEN <kade.allen2@gmail.com>
2022-07-05 11:24:09 -0700

    working on rating.

--- SaucyCapstone/Pages/Admin/Ratings/Index.cshtml.cs
+++ SaucyCapstone/Pages/Admin/Ratings/Index.cshtml.cs
@@ -21,6 +21,7 @@ namespace SaucyCapstone.Pages.Admin.Ratings
 
         // public IList<Rating> Rating { get;set; } = default!;
         public IList<Student> Student { get; set; } = default!;
+        public IList<Criterion> Criterion { get; set; } = default!;
 
         public async Task OnGetAsync()
         {
@@ -28,7 +29,32 @@ namespace SaucyCapstone.Pages.Admin.Ratings
             {
                 //Rating = await _context.Ratings.ToListAsync();
                 Student = await _context.Students.ToListAsync();
+                Criterion = await _context.Criteria.ToListAsync();
+            }
+            if (!_context.Students.Any())
+            {
+                if (!_context.Criteria.Any())
+                {
+                    var criteria = new List<Criterion> { 
+                        new Criterion { Description = "Academics", Weight = new decimal(.25) },
+                        new Criterion { Description = "Age", Weight = new decimal(.05) },
+                        new Criterion { Description = "Distance", Weight = new decimal(.05) },
+                        new Criterion { Description = "Family support and interest", Weight = new decimal(.4) },
+                        new Criterion { Description = "Finanaces", Weight = new decimal(.25) }
+                    };
+                    _context.AddRange(criteria);
+                    _context.SaveChanges();
+                }
+                var students = new List<Student>
+                {
+                    new Student { FirstName = "Karl", LastName = "Guy", DateOfBirth = new DateTime(1990, 7, 7), AcceptedDate = DateTime.Now, LastModifiedDate = DateTime.Now, IsActive = true, Status = new Student.StudentStatus(), SchoolLevel = '1',  FoodAssistance = false, ChappaAssistance = true, Determination = new Student.DeterminationLevel() },
+                };
+                _context.AddRange(students);
+                _context.SaveChanges();
             }
         }
+        //public async Task OnPostAsync(){
+          
+        //}
     }
 }
