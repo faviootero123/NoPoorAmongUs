6b632078c70ec2ec87a3c95b2c4f8b6a5a2f5c42
John Barnett <johnb@cadenceig.com>
2022-07-06 16:43:18 -0600

    update to remove the offending nuget package that is not dotnet 6 compatable.

--- Models/ViewModels/CourseVM.cs
+++ Models/ViewModels/CourseVM.cs
@@ -1,11 +1,6 @@
 ﻿using Data;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Mvc;
+
 
 namespace Models.ViewModels;
 public class CourseVM
