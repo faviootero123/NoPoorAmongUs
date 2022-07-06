1ef80d38b78c2bfa4b886b4773c82a3fc2d6e97d
ams6293 <amberspatig@mail.weber.edu>
2022-07-06 10:41:28 -0600

    added student index functionality

--- SaucyCapstone/Pages/Students/Index.cshtml.cs
+++ SaucyCapstone/Pages/Students/Index.cshtml.cs
@@ -8,56 +8,65 @@ namespace SaucyCapstone.Pages.Students
 {
     public class IndexModel : PageModel
     {
-        private readonly ApplicationDbContext _db;
-        public StudentVM StudentVM { get; set; }
+        private readonly SaucyCapstone.Data.ApplicationDbContext _context;
 
-        public IndexModel(ApplicationDbContext db)
+        public IndexModel(SaucyCapstone.Data.ApplicationDbContext context)
         {
-            _db = db;
-
-            //GetStudentGuardian = _db.StudentGuardians.Where(u => u.Student.StudentId == studentId).FirstOrDefault();
-
-            StudentVM = new StudentVM()
-            {
-                //Students = _db.Students.Where(u => u.StudentId == studentId).FirstOrDefault(),
-                //Guardians = _db.Guardians.Where(u => u.GuardianId == GetStudentGuardian.Guardian.GuardianId).ToList()
-                Students = new Student()
-                {
-                    FirstName = "Alex",
-                    LastName = "Junior",
-                    Phone = "555-555-5555",
-                    DateOfBirth = DateTime.Now,
-                    AcceptedDate = DateTime.Now,
-                    LastModifiedDate = DateTime.Now,
-                    IsActive = true,
-                    Status = Student.StudentStatus.Active,
-                    SchoolLevel = '8',
-                    FoodAssistance = false,
-                    ChappaAssistance = true,
-                    Determination = Student.DeterminationLevel.High,
-                    AnnualIncome = 1333,
-                    Picture = "~/StudentPictures/obama.jpg",
-                    NotesAndAbout = "obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama "
-                },
-                Guardians = new List<Guardian>()
-                 {
-                    new Guardian()
-                    {
-                        FirstName = "Mary",
-                        LastName = "Larry",
-                        ContactInfo = "555-555-5555",
-                        Relation = "Mother"
-                    },
-                    new Guardian()
-                    {
-                        FirstName = "Colt",
-                        LastName = "Bolt",
-                        ContactInfo = "555-555-5555",
-                        Relation = "Father"
-                    }
-                 }
-
-            };
+            _context = context;
         }
+        //public StudentVM StudentVM { get; set; }
+        public IList<Student> Student { get; set; }
+        public void OnGet()
+        {
+            Student = _context.Students.ToList();
+        }
+        //public IndexModel(ApplicationDbContext db)
+        //{
+        //    _db = db;
+
+        //    //GetStudentGuardian = _db.StudentGuardians.Where(u => u.Student.StudentId == studentId).FirstOrDefault();
+
+        //    StudentVM = new StudentVM()
+        //    {
+        //        //Students = _db.Students.Where(u => u.StudentId == studentId).FirstOrDefault(),
+        //        //Guardians = _db.Guardians.Where(u => u.GuardianId == GetStudentGuardian.Guardian.GuardianId).ToList()
+        //        Students = new Student()
+        //        {
+        //            FirstName = "Alex",
+        //            LastName = "Junior",
+        //            Phone = "555-555-5555",
+        //            DateOfBirth = DateTime.Now,
+        //            AcceptedDate = DateTime.Now,
+        //            LastModifiedDate = DateTime.Now,
+        //            IsActive = true,
+        //            Status = Student.StudentStatus.Active,
+        //            SchoolLevel = '8',
+        //            FoodAssistance = false,
+        //            ChappaAssistance = true,
+        //            Determination = Student.DeterminationLevel.High,
+        //            AnnualIncome = 1333,
+        //            Picture = "~/StudentPictures/obama.jpg",
+        //            NotesAndAbout = "obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama obama "
+        //        },
+        //        Guardians = new List<Guardian>()
+        //         {
+        //            new Guardian()
+        //            {
+        //                FirstName = "Mary",
+        //                LastName = "Larry",
+        //                ContactInfo = "555-555-5555",
+        //                Relation = "Mother"
+        //            },
+        //            new Guardian()
+        //            {
+        //                FirstName = "Colt",
+        //                LastName = "Bolt",
+        //                ContactInfo = "555-555-5555",
+        //                Relation = "Father"
+        //            }
+        //         }
+
+        //    };
+        //}
     }
 }
