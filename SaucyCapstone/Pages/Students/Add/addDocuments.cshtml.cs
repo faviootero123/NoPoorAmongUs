f139f8666ea2f78dc643cdfd7594bf2868d66aec
John Barnett <johnb@cadenceig.com>
2022-07-08 19:16:13 -0600

    May have fixed file upload to operating system agnostic.

--- SaucyCapstone/Pages/Students/Add/addDocuments.cshtml.cs
+++ SaucyCapstone/Pages/Students/Add/addDocuments.cshtml.cs
@@ -3,71 +3,73 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SaucyCapstone.Data;
 
-namespace SaucyCapstone.Pages.Students.Add
+namespace SaucyCapstone.Pages.Students.Add;
+
+public class addDocumentsModel : PageModel
 {
-    public class addDocumentsModel : PageModel
+    private readonly ApplicationDbContext _db;
+    private readonly IWebHostEnvironment _hostEnvironment;
+
+    [BindProperty]
+    public int StudentId { get; set; }
+    [BindProperty]
+    public StudentDoc StudentDocs { get; set; }
+
+    public addDocumentsModel(ApplicationDbContext db, IWebHostEnvironment hostEnvironment)
     {
-        private readonly ApplicationDbContext _db;
-        private readonly IWebHostEnvironment _hostEnvironment;
+        _db = db;
+        StudentDocs = new StudentDoc();
+        _hostEnvironment = hostEnvironment;
+    }
 
-        [BindProperty]
-        public int StudentId { get; set; }
-        [BindProperty]
-        public StudentDoc StudentDocs { get; set; }
+    public async Task OnGetAsync(int id)
+    {
+        StudentId = id;
+    }
 
-        public addDocumentsModel(ApplicationDbContext db, IWebHostEnvironment hostEnvironment)
+    public async Task<IActionResult> OnPostAsync(int id, IFormFile? file)
+    {
+        string wwwRootPath = _hostEnvironment.WebRootPath;
+        string ex = "";
+        string guid = "";
+        if (file != null)
         {
-            _db = db;
-            StudentDocs = new StudentDoc();
-            _hostEnvironment = hostEnvironment;
-        }
-
-        public async Task OnGetAsync(int id)
-        {
-            StudentId = id;
-        }
-
-        public async Task<IActionResult> OnPostAsync(int id, IFormFile? file)
-        {
-            string wwwRootPath = _hostEnvironment.WebRootPath;
-            string ex = "";
-            string guid = "";
-            if (file != null)
+            string fileName = Guid.NewGuid().ToString();
+            var uploads = Path.Combine(wwwRootPath, "documents");
+ 
+            if(!Directory.Exists(uploads)) Directory.CreateDirectory(uploads);
+            
+            var extension = Path.GetExtension(file.FileName);
+            if (StudentDocs.Path != null)
             {
-                string fileName = Guid.NewGuid().ToString();
-                var uploads = Path.Combine(wwwRootPath, @"documents\");
-                var extension = Path.GetExtension(file.FileName);
-                if (StudentDocs.Path != null)
+                var oldImagePath = Path.Combine(wwwRootPath, StudentDocs.Path.TrimStart('\\'));
+                if (System.IO.File.Exists(oldImagePath))
                 {
-                    var oldImagePath = Path.Combine(wwwRootPath, StudentDocs.Path.TrimStart('\\'));
-                    if (System.IO.File.Exists(oldImagePath))
-                    {
-                        System.IO.File.Delete(oldImagePath);
-                    }
+                    System.IO.File.Delete(oldImagePath);
                 }
-                using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
-                {
-                    file.CopyTo(fileStreams);
-                }
-                StudentDocs.Path = @"\documents\" + fileName + extension;
-                ex = extension;
-                guid = fileName;
             }
-
-            var newDoc = new StudentDoc()
+            using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
             {
-                Student = _db.Students.Where(d => d.StudentId == id).FirstOrDefault(),
-                AccessType = _db.AccessTypes.FirstOrDefault(),
-                Name = StudentDocs.Name,
-                Description = StudentDocs.Description,
-                Path = StudentDocs.Path,
-                FileGUID = guid,
-                Extension = ex,       
-                UploadDate = DateTime.Now
-            };
-            await _db.AddAsync(newDoc);
-            await _db.SaveChangesAsync();
-            return RedirectToPage("../StudentDocuments", new { id = id });
+                file.CopyTo(fileStreams);
+            }
+            StudentDocs.Path = @"\documents\" + fileName + extension;
+            ex = extension;
+            guid = fileName;
         }
+
+        var newDoc = new StudentDoc()
+        {
+            Student = _db.Students.Where(d => d.StudentId == id).FirstOrDefault(),
+            AccessType = _db.AccessTypes.FirstOrDefault(),
+            Name = StudentDocs.Name,
+            Description = StudentDocs.Description,
+            Path = StudentDocs.Path,
+            FileGUID = guid,
+            Extension = ex,       
+            UploadDate = DateTime.Now
+        };
+        await _db.AddAsync(newDoc);
+        await _db.SaveChangesAsync();
+        return RedirectToPage("../StudentDocuments", new { id = id });
     }
 }
