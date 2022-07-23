e8cea15d345fbab8f34af5b99e3a83c161070d5f
John Barnett <johnb@cadenceig.com>
2022-07-23 11:35:50 -0600

    Removing FacultyMember and replacing it with the applicationuser.

--- Data/ApplicationUser.cs
+++ Data/ApplicationUser.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -8,5 +9,12 @@ namespace Data;
 
 public class ApplicationUser : IdentityUser
 {
+    [Required]
+    public string FirstName { get; set; } = string.Empty;
+    [Required]
+    public string LastName { get; set; } = string.Empty;
 
+    //relationships
+    public IList<Note>? Notes { get; set; }
+    public IList<Course>? Courses { get; set; }
 }
