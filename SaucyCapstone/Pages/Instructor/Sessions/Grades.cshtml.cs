5c48c73395a25bd39dd35a2fbafea2449f2a9dae
ams6293 <amberspatig@mail.weber.edu>
2022-07-24 11:44:38 -0600

    can view assessments and students for each in a session

--- SaucyCapstone/Pages/Instructor/Sessions/Grades.cshtml.cs
+++ SaucyCapstone/Pages/Instructor/Sessions/Grades.cshtml.cs
@@ -2,6 +2,7 @@ using Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using SaucyCapstone.Data;
 
 namespace SaucyCapstone.Pages.Instructor.Sessions;
@@ -15,11 +16,20 @@ public class GradesModel : PageModel
     {
         _context = context;
     }
-    public IList<Assessment> assessments { get; set; } 
+    public IList<Assessment> assessments { get; set; }
+    public List<Enrollment> Enrollments;
+    public Session session { get; set; }
     public void OnGet(int? id)
     {
-        //var session = _context.Sessions.Where(c=> c.SessionId == id).FirstOrDefault();
+        session = _context.Sessions.Include(d => d.Course).ThenInclude(d => d.Term).Include(d => d.Course).ThenInclude(d => d.Subject).Where(d => d.SessionId == id).FirstOrDefault();
 
-        assessments = _context.Assessments.Where(c => c.Course.CourseId == id).ToList();
+        assessments = _context.Assessments.Where(c => c.Course.CourseId == session.Course.CourseId).ToList();
+
+        Enrollments =  _context.Enrollments
+             .Include(d => d.Student)
+             .Where(d => d.SessionId == id)
+             .OrderBy(d => d.Student.LastName)
+             .ThenBy(d => d.Student.FirstName)
+             .ToList();
     }
 }
